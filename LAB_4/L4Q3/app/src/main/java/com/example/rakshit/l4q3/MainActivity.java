7f0987f6696e0a4a37c3e7f60877219eb44dd0a9
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2018-02-05 10:06:25 +0530

    LAB 4 and 3 completed

--- LAB_4/L4Q3/app/src/main/java/com/example/rakshit/l4q3/MainActivity.java
+++ LAB_4/L4Q3/app/src/main/java/com/example/rakshit/l4q3/MainActivity.java
@@ -1,7 +1,7 @@
-package com.example.rakshit.l4q1;
+package com.example.rakshit.l4q3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		ArrayList<String> list = new ArrayList<>();
+		final ArrayList<String> list = new ArrayList<>();
 		list.add("Donut"); list.add("Eclairs"); list.add("Froyo");
 		list.add("Gingerbread"); list.add("Honeycomb"); list.add("Ice Cream Sandwich");
 		list.add("Jelly Bean"); list.add("KitKat"); list.add("Lollipop");
